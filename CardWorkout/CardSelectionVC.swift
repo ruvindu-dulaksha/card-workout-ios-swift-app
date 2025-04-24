df3f331da1778c7347c4b1df543b907402daed03
Ruvindu Dulaksha <146909471+ruvindu-dulaksha@users.noreply.github.com>
2025-04-24 19:23:26 +0530

    upload full project

---
+++ CardWorkout/CardSelectionVC.swift
@@ -0,0 +1,48 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout
+//
+//  Created by Ruvindu Dulaksha on 2025-04-24.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+    
+    @IBOutlet var cardImageView: UIImageView!
+    
+    var cards: [UIImage] = Card.allValues
+    
+    var timer: Timer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startTimer()
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer?.invalidate()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer?.invalidate()
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer?.invalidate()
+        startTimer()
+    }
+    
+    
+   
+}
---
+++ CardWorkoutTests/CardWorkoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  CardWorkoutTests.swift
+//  CardWorkoutTests
+//
+//  Created by Ruvindu Dulaksha on 2025-04-24.
+//
+
+import Testing
+@testable import CardWorkout
+
+struct CardWorkoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
